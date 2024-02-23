811ee59e28574ebcb25fa9a047244326e951c1ac
SurFace81 <sur.f4ce@yandex.ru>
2024-02-23 15:05:12 +0300

    Added work2

---
+++ work2/Matrix.cs
@@ -0,0 +1,175 @@
+﻿namespace MathMatrix
+{
+    internal class Matrix
+    {
+        private int _rows, _cols;
+        private double[,] _data;    // Matrix values
+
+        // Constructor to create a matrix from 1D array
+        public Matrix(int n, int m, double[] values)
+        {
+            _rows = n;
+            _cols = m;
+
+            _data = new double[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    _data[i, j] = values[_rows * i + j];
+                }
+            }
+        }
+
+        // Constructor to create a matrix from 2D array
+        public Matrix(int n, int m, double[,] values)
+        {
+            _rows = n;
+            _cols = m;
+            _data = values;
+        }
+
+        // Method to calculate determinant of the matrix
+        public double det()
+        {
+            if (_rows != _cols)
+                return double.NaN; // Return NaN if matrix is not square. Determinant undefined
+
+            if (_rows == 1)
+            {
+                return _data[0, 0]; // Base case: determinant of 1x1 matrix
+            }
+
+            double res = 0;
+            for (int i = 0; i < _cols; i++)
+            {
+                int n = _rows - 1, m = _cols - 1;
+                double[] temp = new double[n * m];
+                int p = 0;
+                for (int x = 0; x < _rows; x++)     // Loop through rows (?)
+                {
+                    if (x != i)
+                    {
+                        for (int y = 1; y < _cols; y++) // Loop through columns, starting from 1
+                        {
+                            temp[p] = _data[x, y];
+                            p += 1;
+                        }
+                    }
+                }
+
+                Matrix t_matr = new Matrix(n, m, temp);
+                res += Math.Pow(-1, i) * _data[i, 0] * t_matr.det();
+            }
+
+            return res;
+        }
+
+        // Method to calculate transposed matrix
+        public Matrix transp()
+        {
+            double[,] temp = new double[_rows, _cols];
+
+            for (int i = 0; i < _cols; i++)
+            {
+                for (int j = 0; j < _rows; j++)
+                {
+                    temp[i, j] = _data[j, i];
+                }
+            }
+
+            return new Matrix(_rows, _cols, temp);
+        }
+
+        // Method to calculate adjugate of the matrix
+        public Matrix adj()
+        {
+            double[,] temp = new double[_rows, _cols];
+            double deter = det();
+
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _cols; j++)
+                {
+                    temp[i, j] = Math.Pow(-1, i + j) * getMinor(i, j).det() / deter;
+                }
+            }
+
+            return new Matrix(_rows, _cols, temp);
+        }
+
+        // Method to get the minor of the element
+        private Matrix getMinor(int row, int col)
+        {
+            int n = _rows, m = _cols;
+            double[,] minor = new double[n - 1, m - 1];
+
+            int x = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int y = 0;
+                if (i == row)
+                    continue;
+                for (int j = 0; j < m; j++)
+                {
+                    if (j == col)
+                        continue;
+
+                    minor[x, y++] = _data[i, j];
+                }
+                x += 1;
+            }
+
+            return new Matrix(n - 1, m - 1, minor);
+        }
+
+        // Method to calculate inverse matrix
+        public Matrix inv()
+        {
+            return adj().transp();
+        }
+
+        // Method to get the 2D array representing the matrix
+        public double[,] getMatrix()
+        {
+            return _data;
+        }
+
+        // Operator overloading to multiply a matrix by a scalar
+        public static double[,] operator *(Matrix matrxA, double num)
+        {
+            for (int i = 0; i < matrxA._cols; i++)
+            {
+                for (int j = 0; j < matrxA._rows; j++)
+                {
+                    matrxA._data[i, j] *= num;
+                }
+            }
+
+            return matrxA.getMatrix();
+        }
+
+        // Operator overloading to multiply two matrices
+        public static double[,] operator *(Matrix matrxA, Matrix matrxB)
+        {
+            if (matrxA._cols != matrxB._rows)
+                return new double[2, 1] { { double.NaN }, { double.NaN } };
+
+            int n = matrxB._rows, m = matrxB._cols;
+            double[,] res = new double[n, m];
+
+            for (int i = 0; i < matrxA._rows; i++)
+            {
+                for (int j = 0; j < matrxB._cols; j++)
+                {
+                    for (int k = 0; k < matrxA._cols; k++)
+                    {
+                        res[i, j] += matrxA._data[i, k] * matrxB._data[k, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ work2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MathMatrix;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Answer:
+        //  [-1, 5][1, -1]
+        Matrix matrixA = new Matrix(2, 2, new double[] { 3, 7, 2, 8 });
+        Matrix matrixB = new Matrix(2, 2, new double[] { 4, 8, 6, 2 });
+        printMatrix(matrixA.inv() * matrixB);
+        Console.WriteLine();
+
+
+        // Answer:
+        //  [0.58, -1.06, 0.16][-3.14, -1.02, -2.28][3.06, 0.58, 2.12]
+        Matrix matrixC = new Matrix(3, 3, new double[] { 2, 2, 2, 1, -3, 0, -4, -1, 3 });
+        Matrix matrixD = new Matrix(3, 3, new double[] { 1, -3, 0, 10, 2, 7, 10, 7, 8 });
+        printMatrix(matrixC.inv() * matrixD);
+    }
+
+    static void printMatrix(double[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                //Console.Write(matrix[i, j]);
+                Console.Write(Math.Round(matrix[i, j], 2).ToString().PadLeft(5));
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
