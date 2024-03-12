f22781cee50a1fe49eadca6852bb0cb0f891b912
SurFace81 <sur.f4ce@yandex.ru>
2024-03-12 17:57:22 +0300

    work 4 started

---
+++ work4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Random;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // int f0 = Convert.ToInt32(Console.ReadLine());
+        // int f0 = 2363;
+        int f0 = 3170;
+
+        Random.Random rnd = new Random.Random();
+        Console.WriteLine(rnd.randDouble(f0));
+    }
+}
---
+++ work4/Random.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Random
+{
+    internal class Random
+    {
+        public double randDouble(int seed)
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                seed *= seed;
+                string f0 = seed.ToString().Substring(2, 4);
+
+                if (f0[0] == '0')
+                {
+                    var temp = seed.ToString();
+                    int f1 = Convert.ToInt32(leftShift(temp));
+                    int f2 = Convert.ToInt32(rightShift(temp));
+                    seed = f1 + f2;
+                }
+
+                seed = Convert.ToInt32(seed.ToString().Substring(2, 4));
+            }
+
+            return Convert.ToDouble("0," + seed.ToString());
+        }
+
+        private string rightShift(string str)
+        {
+            string temp = str.Substring(0, 2);
+            str = str.Remove(0, 2);
+            return str.Insert(str.Length, temp);
+        }
+
+        private string leftShift(string str)
+        {
+            string temp = str.Substring(str.Length - 2);
+            str = str.Remove(str.Length - 2);
+            return temp + str;
+        }
+    }
+}
